5297136216f738ab00153b089a419c74cced2d43
tundeAz <84148788+tundeAz@users.noreply.github.com>
2021-05-18 21:05:01 +0100

    Add connect.php for db connection

---
+++ connect.php
@@ -0,0 +1,35 @@
+<?php
+
+
+if (isset($_REQUEST['username'])){
+    save_search_results();
+};
+
+function save_search_results(){
+    if(isset($_REQUEST['origin']) && isset($_REQUEST['username']) && isset($_REQUEST['destination']) && isset($_REQUEST['distance_in_kilo']) &&
+    isset($_REQUEST['distance_in_mile']) && isset($_REQUEST['duration_text']) && isset($_REQUEST['taxi_fare'])
+    ){
+        $username = $_REQUEST["username"];
+        $in_kilo = $_REQUEST["distance_in_kilo"];
+        $in_mile = $_REQUEST["distance_in_mile"];
+        $origin = $_REQUEST["origin"];
+        $destination = $_REQUEST["destination"];
+        $duration_text = $_REQUEST["duration_text"];
+        $taxi_price = $_REQUEST["taxi_fare"];
+
+        values = "('$username','$in_kilo','$in_mile','$origin',
+        '$destination','$duration_text','$taxi_price')";
+
+        $sql = "INSERT INTO search_results(username,) VALUES {$values}";
+        $db = mysql_connect() or die ("failed to connect");
+        $result = mysql_query() or die();
+        header('Content-Type: application/json');
+        if ($result){
+            echo json_encode(array("status" => "1"));
+        }else{
+            echo json_encode(mysql_error($db));
+        }
+    }else{
+        echo json_encode("missing input");
+    }
+};
