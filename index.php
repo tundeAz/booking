7cc86146462f41739b186b473c04dfb470b1bc24
tundeAz <84148788+tundeAz@users.noreply.github.com>
2021-05-18 14:13:12 +0100

    Add Calculating distance result function

--- index.php
+++ index.php
@@ -20,13 +20,16 @@
 <body>
     <div class="container">
         <form id="distance_form" action="" method="">
-            <!-- <div class="user-box" >
+            <div class="user-box" >
                 <input class="for-control" id="username" placeholder="Enter Name">
-            </div> -->
+                <input name="username" type="hidden" required/>
+            </div>
+
             <div class="user-box">
                 <input id="from_places" type="text" placeholder="Enter Origin">
                 <input id="origin" type="hidden" name="origin" required/>
             </div>
+
             <div class="user-box">
                 <input id="to_places" type="text" placeholder="Enter Destination">
                 <input id="destination" type="hidden" name="destination" required/>
@@ -34,6 +37,13 @@
             
             <input id="submit" type="submit" value="search"/>
         </form>
+    </div>
+
+    <!-- result -->
+    <div class="container">
+        <div id="result" class="hide">
+
+        </div>
     </div>
 </body>
     <!-- google autocomplete api starts-->
@@ -48,8 +58,35 @@
             google.maps.event.addDomListener(window,'load', function() {
                 var from_places = new google.maps.places.Autocomplete(document.getElementById('from_places'));
                 var to_places = new google.maps.places.Autocomplete(document.getElementById('to_places'));
+
+                google.maps.event.addDomListener(from_places, 'place_changed', function(){
+                    var from_place = from_places.getPlace();
+                    var from_address = from_place.formatted_address;
+                    $('#origin').val(from_address);
+                });
+
+                google.maps.event.addDomListener(to_places, 'place_changed', function(){
+                    var to_place = to_places.getPlace();
+                    var to_address = to_place.formatted_address;
+                    $('#destination').val(to_address);
+                });
         });
     });
+
+    // Calculating distance result
+    function calculateDistance(origin, destination){
+        var DistanceMatrixService = new google.maps.DistanceMatrixService();
+        DistanceMatrixService.getDistanceMatrix({
+            origins: [origin],
+            destinations: [destination],
+            travelMode: google.maps.TravelMode.DRIVING,
+            unitSystem: google.maps.unitSystem.IMPERIAL, //miles & feet.
+            // unitSystem: google.maps.unitSystem.metric, //kilometer & meters.
+            avoidHighways: false,
+            avoidTolls: false
+        }, save_result);
+    }
+
     </script>
     <!-- booking function ends -->
 </html>
